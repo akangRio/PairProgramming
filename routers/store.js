5fbe2b4d879488e86efa41df22d128462eba2e6a
akangRio <fachryo97@gmail.com>
2023-04-05 21:48:23 +0700

    fitur Store Cart Berdasarkan profileID

--- routers/store.js
+++ routers/store.js
@@ -2,6 +2,9 @@ const Controller = require('../controller/controller')
 
 const router = require('express').Router()
 
-router.get('/', Controller.readStore)
+router.get('/:profid', Controller.readStore)
+router.get('/:profid/addtocart/:id', Controller.addToCart)
+router.get('/:profid/decrease/:id', Controller.decOrder)
+router.get('/:profid/increase/:id', Controller.incOrder)
 
 module.exports = router
